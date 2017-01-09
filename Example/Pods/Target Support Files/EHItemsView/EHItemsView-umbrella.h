1aa182b93c1e8a72b9595a5f8521adb6939964ce
Eric Huang <WaterFlowsEast@gmail.com>
2017-01-09 09:21:49 +0800

    update podspec

---
+++ Example/Pods/Target Support Files/EHItemsView/EHItemsView-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double EHItemsViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char EHItemsViewVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-EHItemsView_Tests/Pods-EHItemsView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EHItemsView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_EHItemsView_Tests
+@end
